01cee6edd44229fa8c14b6e17749f4a69b5d007c
Coja <jovan.vuceljic@digitalmind.rs>
2022-03-18 13:02:20 +0100

    removed redux files

--- src/state/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { messagesReducer } from "./messagesReducer";
-
-const reducers = combineReducers({
-	 allMessages: messagesReducer
-})
-
-export default reducers;
